a7c96d9df344d3aa1b4e79a5720a2f18078e77ca
Tejerina Jose Natanael <jtejerina51@gmail.com>
2025-06-08 16:08:12 -0300

    creacion del modo administrador, pagina para crear y algunas correcciones en Home parte 1

--- akenogo/urls.py
+++ akenogo/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from akenogo.settings import BASE_DIR
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('app_akeno.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(BASE_DIR, 'app_akeno/static'))
